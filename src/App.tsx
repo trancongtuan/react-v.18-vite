1225db05c5771cecc88147f89f2168609ce9188f
Nguyen Dang Truong <truong.nd@neo-lab.vn>
2022-10-10 18:51:01 +0700

    develop: UI demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { RouterProvider } from 'react-router-dom';
-import router from 'router';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
+import Main from './layout/Main';
+import 'antd/dist/antd.css';
+import './assets/styles/main.css';
+import './assets/styles/responsive.css';
 
 const queryClient = new QueryClient();
-
 function App() {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Switch>
+            <Main>
+              <Route exact path="/dashboard" component={Home} />
+              <Redirect from="*" to="/dashboard" />
+            </Main>
+          </Switch>
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </div>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,24 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Contact from 'screens/Contact';
 import ErrorPage from 'screens/ErrorPage';
 import Home from 'screens/Home';
-import Service from 'screens/Service';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'contact',
-        element: <Contact />,
-      },
-      {
-        path: 'service',
-        element: <Service />,
-      },
-    ],
   },
 ]);
 
